3a8ca5e5cd822360c0fabd1e2d6d9fce70a4b45a
Andrew Smith <asmith7592@yahoo.com>
2018-11-05 15:10:16 -0800

    finished merge function

--- mergesort.js
+++ mergesort.js
@@ -3,5 +3,22 @@ const split = fullArr => {
   return [fullArr.slice(0, mid), fullArr.slice(mid)];
 }
 
-let test = [1, 5, 3, 8, 2, 6, 9, 7, 4];
-console.log(split(test));
+function merge(arr1, arr2) {
+  let sorted = [];
+
+  while (arr1.length && arr2.length) {
+    let mergedNum = arr1[0] < arr2[0] ? arr1.shift() : arr2.shift();
+    sorted.push(mergedNum)
+  }
+  if (arr1.length) {
+    sorted.push(...arr1);
+  } else if (arr2.length) {
+    sorted.push(...arr2);
+  }
+
+  return sorted;
+}
+
+let test = [1, 2, 4, 8];
+let test2 = [3, 5, 6];
+console.log(merge(test, test2).toString());
